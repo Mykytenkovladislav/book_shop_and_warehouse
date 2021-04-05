60852e5ed3869291e73623b8b8d7901d39d66c9d
Vladyslav Mykytenko <mykytenkovladislav@gmail.com>
2021-04-05 18:53:05 +0300

    Update tasks.py
    Code cleanup

--- shop/store/tasks.py
+++ shop/store/tasks.py
@@ -4,28 +4,6 @@ from celery import shared_task
 from store.models import Book, Genre
 
 
-# @shared_task
-# def book_sync():
-#     url = 'http://warehouse:8002/books'
-#     response = requests.get(url=url).json()
-#
-#     for counter, book in enumerate(response):
-#         if Book.objects.filter(id=book['id']).exists():
-#             continue
-#         else:
-#             Book.objects.create(
-#                 id=book['id'],
-#                 title=book['title'],
-#                 author=book['author'],
-#                 summary=book['summary'],
-#                 isbn=book['isbn'],
-#                 language=book['language'],
-#                 genre=book['genre'],
-#                 price=book['price'],
-#             )
-#     print('Sync is done')
-
-
 @shared_task
 def book_sync():
     url = 'http://warehouse:8002/books'
